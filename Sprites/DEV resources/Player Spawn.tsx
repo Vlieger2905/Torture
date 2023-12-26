d1c8b1e4e6ef64b940fdda0fde18df2a2942cfe4
Vlieger2905 <allard295@gmail.com>
2023-12-26 14:26:12 +0100

    modified:   Map Data/Test Map/testMap.tmx 	modified:   Player/__pycache__/player.cpython-312.pyc 	modified:   Sprites/DEV resources/Player Spawn.tsx

--- Sprites/DEV resources/Player Spawn.tsx
+++ Sprites/DEV resources/Player Spawn.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Player Spawn" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../Sprites/DEV resources/Player Spawn.png" width="32" height="32"/>
+ <image source="Player Spawn.png" width="32" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="Spawnpoint" value=""/>
+  </properties>
+ </tile>
 </tileset>
