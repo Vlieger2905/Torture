a110ac3fd797b68ab142d5a4975d6d442f9f7255
Vlieger2905 <allard295@gmail.com>
2024-03-30 22:34:25 +0100

    Fixed the sensory to not go beyond the closest obstacle

--- Enemies/Enemy.py
+++ Enemies/Enemy.py
@@ -39,4 +39,33 @@ class Enemy(Sprite):
                     elif self.direction.y < 0: #Moving up
                         self.hitbox.top = sprite.hitbox.bottom
     
+    # Function to change the length of the sensory lines to either the detection range if there is not an obstacle in the way. Or to the side of the closest obstacle.
+    def obstacle_detection(self):
+        index = 0
+        # for each sensor line check whether they collide with any object
+        for line in self.sensory_lines:
+            updated = False
+            for object in self.obstacle_sprites:
+                # Save the result in endpoint
+                end_point = self.collide_detect(line, object)
+            
+                # If there is a collision update the line to the new line
+                if end_point != None:
+                    line = (line[0], end_point, self.no_detection_colour)
+                    updated = True
+            if updated:
+                self.sensory_lines[index] = line
+            index += 1
+                
+
+    def collide_detect(self, line, obstacle):
+        # Use clipline to check for collision
+        clipped_line = obstacle.rect.clipline(line[0], line[1])
+
+        if clipped_line:
+            # If clipped_line is not an empty tuple, then the line collides/overlaps with the rect
+            start, end = clipped_line
+            return start  # Return the first point where the line intersects with the rect
+        else:
+            return None  # Return None if no collision is detected
     
--- Enemies/slime.py
+++ Enemies/slime.py
@@ -33,6 +33,7 @@ class Slime(Enemy):
         starting_position = self.rect.center
         self.no_detection_colour = (0,0,255)
         self.detected_colour = (255,0,0)
+
         # Calculate coordinates for each endpoint
         for i in range(amount_of_sensory_lines):
             # Getting the point from the unit circle
@@ -51,10 +52,18 @@ class Slime(Enemy):
 
     def update(self,dt):
         if self.first_frame is False:
-            self.move(dt)
+            # Try and find something
             self.looking_around()
+
+            # If you found something go to the something
+
+            # Other wise just keep waddling around
+
+            # Move the enemy
+            self.move(dt)
         else:
             self.first_frame = False
+
 # A function that looks around the enemy and correctly reacts
     def looking_around(self):
         # Resetting the all the sensory lines to their original state
@@ -62,36 +71,3 @@ class Slime(Enemy):
         # Putting the limit of each obstacle sprite to the start of a obstacle
         self.obstacle_detection()
 
-# TODO fix it that the endpoint will only get clipped to the smallest distance
-    def obstacle_detection(self):
-        index = 0
-        # for each sensor line check whether they collide with any object
-        for line in self.sensory_lines:
-            for object in self.obstacle_sprites:
-                # Save the result in endpoint
-                end_point = self.collide_detect(line, object)
-                
-                # If there is a collision the endpoint should not be none.
-                if end_point != None:
-                    self.sensory_lines[index] = (line[0], end_point, self.no_detection_colour)
-                    # break
-            index += 1
-                
-
-    def collide_detect(self, line, obstacle):
-        # Use clipline to check for collision
-        clipped_line = obstacle.rect.clipline(line[0], line[1])
-
-        if clipped_line:
-            # If clipped_line is not an empty tuple, then the line collides/overlaps with the rect
-            start, end = clipped_line
-            return start  # Return the first point where the line intersects with the rect
-        else:
-            return None  # Return None if no collision is detected
-    
-    # Function to calculate the distance between two points
-    def distance_between_points(point1, point2):
-        x1, y1 = point1
-        x2, y2 = point2
-        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-        return distance
